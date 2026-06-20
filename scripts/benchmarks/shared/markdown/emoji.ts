e873edc34015ed85cd05ff27e51df7af239ff6c1
Jay Lee <jay@vendit.co.kr>
2026-06-20 19:13:02 +0900

    chore: fix emojis background (#39)

--- scripts/benchmarks/shared/markdown/emoji.ts
+++ scripts/benchmarks/shared/markdown/emoji.ts
@@ -4,5 +4,5 @@ const BASE_URL =
 export function fluentEmoji(category: string, name: string, size = 25): string {
   const path = `${category}/${name}.png`.replace(/ /g, '%20');
 
-  return `<img src="${BASE_URL}/${path}" alt="${name}" width="${size}" height="${size}" style="vertical-align: middle" />`;
+  return `<img src="${BASE_URL}/${path}" alt="${name}" width="${size}" height="${size}" style="vertical-align: middle; background: none" />`;
 }
